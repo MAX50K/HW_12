12b8d146841ff200d3ab277cc91385bba20519d9
MAX50K <146106579+MAX50K@users.noreply.github.com>
2025-04-18 14:11:34 +0200

    ee

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 const user = {
-  
+    name: "John",
+    age: 30,
+    hobby: "reading",
+    premium: true
+};
+
+user.mood = "happy";
+
+user.hobby = "skydiving";
+
+user.premium = false;
+
+const keys = Object.keys(user);
+
+for (const key of keys) {
+    console.log(`${key}: ${user[key]}`);
+}
+// 2
+function countProps(obj) {
+    return Object.keys(obj).length;
 }
 
-function userAdds(mood, hobby, premium) {
-    user.mood = "happy"
+const user = {
+    name: "Max",
+    age: 12,
+    hobby: "programming",
+    isStudent: true
+};
 
-}
-
-console.log(user);
+console.log(countProps(user));
